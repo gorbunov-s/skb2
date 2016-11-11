7b9744ed52d57c5ddb53ff53f40d604b409c406b
Sergey Gorbunov <Sergey Gorbunov>
2016-11-12 01:07:05 +0400

    Задание 2В

--- src/index.js
+++ src/index.js
@@ -2,18 +2,56 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
-app.get('/', (req, res) => {
-	res.json({
-		hello: 'JS World'
-	});
-});
 
-app.get('/task2A', (req, res) => {
-    const sum = (+req.query.a || 0) + (+req.query.b || 0);
-    res.send(sum.toString());
+
+function handleFullName(fullname) {
+    return fullname.replace(/\s\s+/g, ' ').split(' ')
+}
+
+function handleFirstName(namePart) {
+    return `${ namePart.charAt(0).toUpperCase() }${ namePart.slice(1).toLowerCase() }`
+}
+
+function handleOther(namePart) {
+    return `${ namePart.charAt(0).toUpperCase() }.`
+}
+
+function tests(queryFullname, fullnameArray) {
+    if (!queryFullname || fullnameArray.length > 3) {
+        return true;
+    }
+
+    for (let namePart = 0; namePart < fullnameArray.length; namePart++) {
+        if (!!~fullnameArray[namePart].search(/\d|[_/]/g)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+app.use(cors());
+
+app.get('/task2B', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://account.skill-branch.ru");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+    const fullnameArray = handleFullName(req.query.fullname);
+
+    if (tests(req.query.fullname, fullnameArray)) {
+        res.send('Invalid fullname');
+    } else {
+        let refultFullname = handleFirstName(fullnameArray[fullnameArray.length - 1]);
+
+        fullnameArray.slice(0, -1).forEach((partName) => {
+            refultFullname += ` ${ handleOther(partName) }`;
+        });
+
+        res.send(refultFullname);
+    }
 });
 
 app.listen(3000, () => {
-	console.log('Your app listening on port 3000!');
+    console.log('Your app Listening on port 3000!');
 });
